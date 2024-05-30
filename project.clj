b771ed6d6ce19863c5ecbccbdfc2394aa6039ba9
Circle CI <circleci@logicblocks.io>
2024-05-30 12:07:13 +0000

    Pre-release version 0.1.0-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/buddy.graalvm "0.1.0-RC0"
+(defproject io.logicblocks/buddy.graalvm "0.1.0-RC1"
   :description "Support functions for other logicblocks components."
   :url "Definitions to enable using buddy with GraalVM native images."
 
